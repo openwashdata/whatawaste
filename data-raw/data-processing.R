47366024125ac21eb32aef40d59153bbdfe4e834
Mian <mzhong@ethz.ch>
2024-03-26 09:14:04 +0100

    update description and draft of data-processing functions

--- data-raw/data-processing.R
+++ data-raw/data-processing.R
@@ -8,7 +8,41 @@ countrylevel <- countrylevel |>
   dplyr::mutate(dplyr::across(c("region_id", "income_id"), as.factor)) |>
   dplyr::mutate(dplyr::across(starts_with("other_information_"), as.factor))
 
-skimr::skim(citylevel)
+##
+extract_group <- function(dataset, prefix, suffix = "percent") {
+  if (suffix == "") {
+    dataset |>
+      dplyr::select(c("iso3c", starts_with(prefix))) |>
+      tidyr::pivot_longer(
+        cols = starts_with(prefix),
+        names_to = "type",
+        names_pattern = paste0(prefix, "(.*)_")
+      )
+  } else {
+    dataset |>
+      dplyr::select(c("iso3c", starts_with(prefix))) |>
+      tidyr::pivot_longer(
+        cols = starts_with(prefix),
+        names_to = "type",
+        names_pattern = paste0(prefix, "(.*)_", suffix),
+        values_to = suffix
+      )
+  }
+}
+
+extract_country_group <- function(prefix, suffix = "percent") {
+  extract_group(countrylevel, prefix, suffix)
+}
+
+extract_city_group <- function(prefix, suffix = "percent") {
+  extract_group(citylevel, prefix, suffix)
+}
+
+country_composition <- extract_country_group(prefix = "composition_")
+country_specialwaste <- extract_country_group(prefix = "special_waste_", suffix = "tons_year")
+country_wastetreatment <- extract_country_group(prefix = "waste_treatment_")
+country_collectcov <- extract_country_group(prefix = "waste_collection_coverage_", suffix = "")
+
 # CITY LEVEL DATA --------------------------------------------------------------
 citylevel <- readr::read_csv("data-raw/city_level_data_0_0.csv")
 ## Modify data types -----------------------------------------------------------
