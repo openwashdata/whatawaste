c69a32670ef9f72170ec3b5040ef3d317d118f00
Nicolo Massari <nmassari@ethz.ch>
2025-03-10 16:14:57 +0100

    Data documentation

---
+++ R/countrylevel.R
@@ -0,0 +1,24 @@
+#' countrylevel: What a Waste Country level
+#'
+#' What a Waste is a global project to aggregate data on solid waste management
+#  from around the world. This database features the statistics collected
+#  through the effort, covering nearly all countries and over 330 cities.
+#  The metrics included cover all steps from the waste management value chain,
+#  including waste generation, composition, collection, and disposal, as well as
+#  information on user fees and financing, the informal sector, administrative
+#  structures, public communication, and legal information. The information
+#  presented is the best available based on a study of current literature and
+#  limited conversations with waste agencies and authorities. While there may be
+#  variations in the definitions and quality of reporting for individual data
+#  points, general trends should reflect the global reality. All sources and any
+#  estimations are noted.
+#'
+#' @format A tibble with 217 rows and 5 variables
+#' \describe{
+#'   \item{iso3c}{NA}
+#'   \item{region_id}{NA}
+#'   \item{country_name}{NA}
+#'   \item{income_id}{NA}
+#'   \item{gdp}{NA}
+#' }
+"countrylevel"
