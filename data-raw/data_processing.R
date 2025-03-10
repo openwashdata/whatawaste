1b0c55531b6f66d84609953cabe766e0d30a6ed7
Nicolo Massari <nmassari@ethz.ch>
2025-03-10 16:25:44 +0100

    Created whatawaste library

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -12,19 +12,20 @@ library(openxlsx)
 library(dplyr)
 
 # Read data --------------------------------------------------------------------
-data_in <- readr::read_csv("data-raw/country_level_data.csvins")
+data_in <- readr::read_csv("data-raw/country_level_data.csvins") %>%
+  janitor::clean_names()
 # codebook <- readxl::read_excel("data-raw/codebook.xlsx") |>
 #  clean_names()
 
 # Tidy data --------------------------------------------------------------------
 ## Clean the raw data into a tidy format here
-data_in %>%
-  janitor::clean_names()
+countrylevel <- data_in %>%
+  select(iso3c:gdp)
 
 # Export Data ------------------------------------------------------------------
-usethis::use_data(whatawaste, overwrite = TRUE)
+usethis::use_data(countrylevel, overwrite = TRUE)
 fs::dir_create(here::here("inst", "extdata"))
-readr::write_csv(whatawaste,
-                 here::here("inst", "extdata", paste0("whatawaste", ".csv")))
-openxlsx::write.xlsx(whatawaste,
-                     here::here("inst", "extdata", paste0("whatawaste", ".xlsx")))
+readr::write_csv(countrylevel,
+                 here::here("inst", "extdata", paste0("countrylevel", ".csv")))
+openxlsx::write.xlsx(countrylevel,
+                     here::here("inst", "extdata", paste0("countrylevel", ".xlsx")))
